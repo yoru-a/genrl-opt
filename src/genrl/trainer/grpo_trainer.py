d15a4be8eb5dfad1f9a04f2a85f94dd99ec43677
ishizora <72289147+zora0x@users.noreply.github.com>
2025-10-20 12:26:25 +0530

    0

--- src/genrl/trainer/grpo_trainer.py
+++ src/genrl/trainer/grpo_trainer.py
@@ -344,7 +344,7 @@ class GRPOLanguageTrainerModule(TrainerModule, LoggerMixin):
                 temperature=self.args.temperature,
                 top_p=self.args.top_p,
                 top_k=self.args.top_k if self.args.top_k is not None else -1,
-                min_p=self.args.min_p if self.args.min_p is not None else -1,
+                min_p=self.args.min_p if self.args.min_p is not None else 0,
                 max_tokens=self.args.max_new_tokens,
                 repetition_penalty=self.args.repetition_penalty,
             )
